0eb1719d7b7241979184dd474eee3fcb4a2cd003
Thauani Rodrigues <134622011+ThataRodrigues@users.noreply.github.com>
2023-10-05 11:30:26 -0300

    Update exercício13.java

--- aula01/exercício13.java
+++ aula01/exercício13.java
@@ -1,31 +1,23 @@
 import java.util.Scanner;
 
-public class exercise13 {
-
-    /**
-     * Exercicio 13:
-     * Escreva um programa que calcule o fatorial de um número inteiro não negativo fornecido pelo
-     * usuário. O fatorial de um número N é o produto de todos os inteiros de 1 até N.
-     */
+public class exercicio13 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite um número inteiro:");
-        int number = scanner.nextInt();
+        System.out.println("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
 
-        if (number >= 0) {
-            long factorial = 1;
+        if (numero >= 0) {
+            long fatorial = 1;
 
-            for (int i = 1; i <= number; i++) {
-                factorial *= i;
+            for (int i = 1; i <= numero; i++) {
+                fatorial *= i;
             }
-            System.out.println("O fatorial de " + number + " é: " + factorial);
-
-        } else {
-        System.out.println("O número não pode ser negativo.");
+            System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        }else{
+            System.out.println("O número não pode ser negativo.");
         }
-
         scanner.close();
     }
 }
