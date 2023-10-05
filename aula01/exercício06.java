b77b32c814ab1daba12eed21328078287996be2a
Thauani Rodrigues <134622011+ThataRodrigues@users.noreply.github.com>
2023-10-05 11:27:25 -0300

    Update exercício06.java

--- aula01/exercício06.java
+++ aula01/exercício06.java
@@ -1,31 +1,20 @@
-package com.github.javacaldeira;
-
 import java.util.Scanner;
 
-public class exercise06 {
+public class exercicio06{
 
-    /**
-     * Exercicio 06: Escreva um programa que leia a idade do usuário e imprima respostas diferentes
-     * dependendo do número colocado:
-     * Se for menor de 16 anos, avisar que não pode votar
-     * Se tiver 16 ou 17, avisar que o voto é facultativo
-     * Se tiver mais de 65, avisar que também é facultativo o voto
-     * De 18 até 65, é obrigatório votar.
-     */
-
-    public static void main(String[] args) {
-
+    public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite sua idade: ");
-        int age = scanner.nextInt();
+        int idade = scanner.nextInt();
 
-        if (age < 16) {
+        if( idade<16 ){
             System.out.println("Você não pode votar.");
-        } else if (age <= 17 || age > 65) {
+        }else if( idade <= 17 || idade > 65){
             System.out.println("O voto é facultativo.");
-        } else {
-            System.out.println("O voto é obrigatório.");
+        }else{
+            System.out.println("O voto é origatório.");
+
         }
         scanner.close();
     }
