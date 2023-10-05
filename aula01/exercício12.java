9fc47cffc3d77ff0810689f5b1e1aa1df1ecba33
Thauani Rodrigues <134622011+ThataRodrigues@users.noreply.github.com>
2023-10-05 11:29:11 -0300

    Update exercício12.java

--- aula01/exercício12.java
+++ aula01/exercício12.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
 
-public class exercise12 {
+public class exercicio12{
 
-    /**
-     * Exercicio 12:
-     * Crie um programa que converta uma quantia em dólares para outra moeda, como euros ou reais.
-     * Peça ao usuário para inserir a quantia em dólares e a taxa de câmbio atual.
-     * Em seguida, calcule e exiba o valor convertido.
-     */
-
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a quantia em dólares:");
-        double dollarAmount = scanner.nextDouble();
+        System.out.println("Digite um valor em dólares: ");
+        double dolar = scanner.nextDouble();
 
-        System.out.println("Digite a taxa de câmbio:");
-        double exchangeRate = scanner.nextDouble();
+        System.out.println("Digite a taxa de câmbio: ");
+        double taxaCambio = scanner.nextDouble();
 
-        double convertedAmount = dollarAmount * exchangeRate;
+        double valorConvertido = dolar * taxaCambio;
 
-        System.out.println("A quantia convertida é: " + convertedAmount);
+        System.out.println("A quantia convertida é: "+ valorConvertido);
 
         scanner.close();
     }
+
 }
