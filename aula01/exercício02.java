41029320199300324994100f7ab4a30b4a2e296c
Thauani Rodrigues <134622011+ThataRodrigues@users.noreply.github.com>
2023-10-05 11:25:51 -0300

    Update exercício02.java
    retomando e atualizando

--- aula01/exercício02.java
+++ aula01/exercício02.java
@@ -1,28 +1,19 @@
 import java.util.Scanner;
 
-public class exercise02 {
-
-    /**
-     * Exercicio 02:
-     * Crie um programa que receba 2 números e imprima na tela.
-     */
-
-    public static void main(String[] args) {
-
+public class exercicio02{
+    public static void main(String[]args){
         double primeiroNumero;
         double segundoNumero;
 
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Digite o primeiro número: ");
-
         primeiroNumero = scanner.nextDouble();
 
         System.out.println("Digite o segundo número: ");
         segundoNumero = scanner.nextDouble();
 
-        System.out.println("Primeiro número é: " + primeiroNumero);
-        System.out.println("Segundo número é: " + segundoNumero);
+        System.out.println("Primeiro número é : "+primeiroNumero);
+        System.out.println("Segundo número é: "+segundoNumero);
 
         scanner.close();
     }
