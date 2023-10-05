f83e0f0bd380fa490ea60bcf1e4e4c1c9caeb9e1
Thauani Rodrigues <134622011+ThataRodrigues@users.noreply.github.com>
2023-10-05 11:27:41 -0300

    Update exercício07.java

--- aula01/exercício07.java
+++ aula01/exercício07.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
 
-public class exercise07 {
+public class exercicio07{
 
-    /**
-     * Exercicio 07:
-     * Criar para uma empresa de consórcio. No teste inicial, você deve perguntar
-     * para o usuário a idade dele e o seu salário. A pessoa só pode comprar um automóvel
-     * se for de maior e ganhar mais de R$ 2.000,00.
-     */
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite sua idade: ");
-        int age = scanner.nextInt();
+        int idade = scanner.nextInt();
 
         System.out.println("Digite seu salário: ");
-        double wage = scanner.nextDouble();
+        double salario = scanner.nextDouble();
 
-        if (age >= 18 && wage > 2000.00) {
-            System.out.println("Você pode comprar um automóvel!!");
-        } else {
-            System.out.println("Você é menor de idade ou não tem a verba suficiente :/");
+        if( idade >=18 && salario >2000.00){
+            System.out.println("Pode comprar um automóvel!!");
+        }else{
+            System.out.println("Não pode comprar um automóvel. Você tem menos que 18 anos ou não tem verba o suficiente." );
         }
-        scanner.close();
+       scanner.close();
     }
 }
