ff86bafb14d368b948c6d94978f071423460435b
Thauani Rodrigues <134622011+ThataRodrigues@users.noreply.github.com>
2023-10-05 11:28:37 -0300

    Update exercício10.java

--- aula01/exercício10.java
+++ aula01/exercício10.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
 
-public class exercise10 {
+public class exercicio10{
 
-    /**
-     * Exercício 10:
-     * Crie um programa em Java que peça ao usuário para inserir uma temperatura em graus Celsius e,
-     * em seguida, converta essa temperatura em graus Fahrenheit usando a fórmula:
-     * Fahrenheit = (Celsius * 9/5) + 32.
-     * Exiba o resultado na tela.
-     */
-
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a temperatura em graus Celsius:");
+        System.out.println("Digite a temperatura em Graus Celsius: ");
         double celsius = scanner.nextDouble();
 
-        double fahrenheit = (celsius * 9 / 5) + 32;
+        double fahrenheit =  (celsius * 9 / 5)+ 32;
 
-        System.out.println("A temperatura em graus Fahrenheit é: " + fahrenheit);
+        System.out.println("A temperatura em Graus Fahrenheit é: "+ fahrenheit);
 
         scanner.close();
     }
+
 }
